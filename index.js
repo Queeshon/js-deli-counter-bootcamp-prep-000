fab179a0bcbe4ee40669688447187fd375b00881
Christian Kim <christiandwkim@gmail.com>
2018-02-10 17:17:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function currentLine(katzDeliLine){
   if (!katzDeliLine.length){
     return ("The line is currently empty.")
   }
-  const pastrami = []
+  const pastrami = "The line is currently: "
 
-  for (var i = 0; i < katzDeliLine; i++){
-    pastrami.push (`${i + 1}. ${line[i]}`)
+  for (var i = 0; i < katzDeliLine-1; i++){
+    pastrami += (`${i + 1}. ${line[i]}`) + ', '
   }
-  return (`The line is currently: ${pastrami.join(', ')}`)
+  pastrami += (`${katzDeliLine.length}. ${line[katzDeliLine.length-1]}`)
 }
